22e532ccafb6cbe18b5d0ed00cf0e1540693edc8
justrmarks <blackbirdwanderer@gmail.com>
2019-08-30 09:31:27 -0500

    solution

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,37 @@
 class Zoo
+  @@all = []
+  attr_accessor :name, :location
+
+  def initialize(name, location)
+    self.name = name
+    self.location = location
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_animal(species,weight,nickname)
+    Animal.new(species,weight,nickname, self)
+  end
+
+
+  def animals
+    Animal.all.select {|animal| animal.zoo == self}
+  end
+
+  def animal_species
+    self.animals.collect {|animal| animal.species}.uniq
+  end
+
+  def animal_nicknames
+    self.animals.collect {|animal| animal.nickname}
+  end
+
+  def self.find_by_location(location)
+    self.all.select {|zoo| zoo.location == location}
+  end
+
 
 end
