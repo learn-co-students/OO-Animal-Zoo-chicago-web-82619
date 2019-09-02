a05a76acd40a5fd6d15705da5d01bf3b5cc84e94
Andrew <andrewriznykua93@gmail.com>
2019-09-01 22:12:39 -0500

    Done.

--- run.rb
+++ run.rb
@@ -4,6 +4,18 @@ require 'pry'
 
 
 #Test your code here
+brookfield = Zoo.new("Brookfield Zoo", "Brookfield")
+bronx = Zoo.new("Bronx Zoo", "New York")
+columbus = Zoo.new("Columbus Zoo", "Powell")
+
+hugo = Animal.new("Hugo", "elephant", 6000, brookfield)
+bernard = Animal.new("Bernard", "bear", 800, bronx)
+davina = Animal.new("Davina", "tiger", 370, brookfield)
+carlo = Animal.new("Carlo", "lion", 420, columbus)
+dingo = Animal.new("Dingo", "wolf", 180, bronx)
+
+
+
 
 
 binding.pry
